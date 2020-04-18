738008dc6de8a715a3e6f8b34e6d6a06a84175be
DavidNeiOs <davidneios25@gmail.com>
2020-04-17 22:04:25 -0400

    Detail screen

--- src/screens/ProductDetailsScreen/ProductDetailsScreen.tsx
+++ src/screens/ProductDetailsScreen/ProductDetailsScreen.tsx
@@ -6,10 +6,12 @@ import {
   Image,
   Button,
   ScrollView,
+  ActivityIndicator,
 } from "react-native";
 
 import { ProductsNavProps } from "../../navigation/ShopNavigator";
 import { useTypedSelector } from "../../store";
+import { Colors } from "../../constants/colors";
 
 interface ProductDetailsScreenProps
   extends ProductsNavProps<"ProductDetails"> {}
@@ -21,11 +23,40 @@ export const ProductDetailsScreen: React.FC<ProductDetailsScreenProps> = ({
   const selectedProduct = useTypedSelector((state) =>
     state.products.availableProducts.find((prod) => prod.id === productId)
   );
-  return (
+  return selectedProduct ? (
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+      <View style={styles.actions}>
+        <Button color={Colors.primary} title="Add to cart" onPress={() => {}} />
+      </View>
+      <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+      <Text style={styles.description}>${selectedProduct.description}</Text>
+    </ScrollView>
+  ) : (
     <View>
-      <Text>{selectedProduct?.title}</Text>
+      <ActivityIndicator size="large" />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: "center",
+  },
+  price: {
+    fontSize: 20,
+    color: "#888",
+    textAlign: "center",
+    marginVertical: 20,
+  },
+  description: {
+    fontSize: 14,
+    textAlign: "center",
+    marginHorizontal: 20,
+  },
+});
